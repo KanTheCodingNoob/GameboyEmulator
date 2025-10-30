ea55cc74249fd352be88c775b01038e079779406
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-10-31 01:39:58 +0700

    Add Mode 2 Operation based on per-clock basis

--- Header/PPU.h
+++ Header/PPU.h
@@ -5,6 +5,15 @@
 #pragma once
 #include <array>
 #include <cstdint>
+#include <queue>
+
+struct OBJ
+{
+    uint8_t yPosition; // Byte 0
+    uint8_t xPosition; // Byte 1
+    uint8_t tileIndex; // Byte 2
+    uint8_t attributes; // Byte 3
+};
 
 class Bus;
 
@@ -13,14 +22,15 @@ public:
     explicit PPU(Bus* bus);
 
     const std::array<uint32_t, 4> colorPalette = {0x0f380f, 0x306230, 0x8bac0f, 0x9bbc0f};
-    const std::array<std::array<uint8_t, 256>, 256> LCD{};
+    const std::array<std::array<uint32_t, 160>, 144> LCD{};
 
     uint16_t returnVRAMAddress(uint8_t addr, bool object);
     std::array<uint8_t, 8> returnPixelValuesFromTwoBytes(uint8_t topLine, uint8_t secondLine);
     ~PPU() = default;
 
     uint32_t bgColorHandler(uint8_t value);
-
+    // M-cycle clock
+    void clock();
 private:
     Bus* bus;
 
@@ -36,4 +46,17 @@ private:
     uint8_t& OBP1;
     uint8_t& WY;
     uint8_t& WX;
+
+    std::queue<uint8_t> pixelFIFO;
+    std::vector<OBJ> OAMInALine;
+
+    uint8_t mode = 2;
+    // Rendering mode
+    // Mode 2
+    void OAMSearch();
+    // Track the amount of cycle passed within OAM Operation
+    uint8_t OAMCycle = 0;
+
+    // Mode 3
+    void PixelTransfer(const std::vector<OBJ>& objs);
 };
--- Source/PPU.cpp
+++ Source/PPU.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "../Header/PPU.h"
+
+#include <algorithm>
+
 #include "../Header/Bus.h"
 
 PPU::PPU(Bus* bus): bus(bus), LCDC (bus->IORegisters[0x40]),
@@ -42,7 +45,59 @@ std::array<uint8_t, 8> PPU::returnPixelValuesFromTwoBytes(const uint8_t topLine,
     return pixels;
 }
 
-uint32_t PPU::bgColorHandler(const uint8_t value)
+uint32_t PPU::bgColorHandler(uint8_t value)
 {
-    return colorPalette[(BGP >> (value * 2) & 0x03)];
-};
+}
+
+void PPU::clock()
+{
+    switch (mode)
+    {
+        // OAM Scan
+        case 2:
+            OAMSearch();
+            break;
+        case 3:
+            break;
+    }
+}
+
+// Scan 2 OBJ per clock
+void PPU::OAMSearch()
+{
+    // Loop through all OAM Y-Position
+    for (int i = OAMCycle * 2 * 4; i < (OAMCycle * 2 + 2) * 4; i = i + 4)
+    {
+        // Check if the OBJ Y position is within the scanline
+        if (LY + 16 >= bus->OAM[i] && LY + 16 < bus->OAM[i] + 8 * (((LCDC & 0x4) >> 2) + 1))
+        {
+            OAMInALine.push_back({bus->OAM[i], bus->OAM[i + 1], bus->OAM[i + 2], bus->OAM[i + 3]});
+        }
+        // Stop after 10 OBJ
+        if (OAMInALine.size() == 10)
+        {
+            break;
+        }
+    }
+    OAMCycle++;
+    if (OAMCycle == 10)
+    {
+        OAMCycle = 0;
+        // Stable sort them by x position for the Pixel Transfer operation
+        std::stable_sort(OAMInALine.begin(), OAMInALine.end(), [](const OBJ& a, const OBJ& b)
+        {
+            return a.xPosition < b.xPosition;
+        });
+        mode = 3;
+    }
+}
+
+void PPU::PixelTransfer(const std::vector<OBJ>& objs)
+{
+    int index = 0;
+    // Get Tile
+    // Get Tile Data Low
+    // Get Tile Data High
+    // Push
+    // Sleep
+}
