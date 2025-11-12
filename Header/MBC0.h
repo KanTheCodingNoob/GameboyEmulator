6f6236cf8d6ab0492bf627a5528c641b3428e8d9
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-11-12 21:58:03 +0700

    Added MBC0

---
+++ Header/MBC0.h
@@ -0,0 +1,15 @@
+//
+// Created by ADMIN on 10/5/2025.
+//
+
+#pragma once
+#include "MBC.h"
+
+
+class MBC0: public MBC {
+public:
+    MBC0();
+    ~MBC0() override;
+
+    bool MBCWrite(uint16_t addr, uint8_t data) override;
+};
---
+++ Source/MBC0.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ADMIN on 11/12/2025.
+//
+
+#include "../Header/MBC0.h"
+
+MBC0::MBC0()
+= default;
+
+MBC0::~MBC0()
+= default;
+
+
+bool MBC0::MBCWrite(uint16_t addr, uint8_t data)
+{
+    return true;
+}
