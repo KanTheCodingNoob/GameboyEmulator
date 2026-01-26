7e2d03d5cb7e1bd2c2549d379141669221098f55
KanTheCodingNoob <s4019548@rmit.edu.vn>
2026-01-27 01:12:13 +0700

    Add comment

--- Core/MBC.h
+++ Core/MBC.h
@@ -11,9 +11,9 @@ public:
     virtual ~MBC() = default;
 
     bool mode = true; // true = default mode, false = alternate mode
-    bool ramEnabled = false;
-    uint8_t romBankNum = 1;
-    uint8_t ramBankNum = 0;
+    bool ramEnabled = false; // Whether external ram is used
+    uint8_t romBankNum = 1; // ROM bank number
+    uint8_t ramBankNum = 0; // RAM bank number
 
     virtual bool MBCWrite(uint16_t addr, uint8_t data) = 0;
 };
