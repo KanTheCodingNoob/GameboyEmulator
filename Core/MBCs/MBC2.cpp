ea762ee77881ad80c85b05ea5c7d5ca7a9da7128
KanTheCodingNoob <s4019548@rmit.edu.vn>
2026-02-07 19:55:25 +0700

    Slight Refactor

---
+++ Core/MBCs/MBC2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ADMIN on 2/7/2026.
+//
+
+#include "MBC2.h"
---
+++ Core/MBCs/MBC2.h
@@ -0,0 +1,14 @@
+//
+// Created by ADMIN on 2/7/2026.
+//
+
+#ifndef GAMEBOYEMU_MBC2_H
+#define GAMEBOYEMU_MBC2_H
+
+
+class MBC2
+{
+};
+
+
+#endif //GAMEBOYEMU_MBC2_H
